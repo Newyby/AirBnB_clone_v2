285fb3c08a9b88acd31b2c7cb79eaaa01d0a9d3d
Akinsanmi omobonike <officialraydata@gmail.com>
2023-12-06 14:44:29 -0800

    Tasks

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Fabric script that distributes an archive to web servers """
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/school -u ubuntu
 from fabric.api import *
 from datetime import datetime
 import os
